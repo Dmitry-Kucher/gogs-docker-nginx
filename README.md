4c3c00028fd31670c34dac76331a2fb648e7e9c1
Dima-macbook <dima@virtuman.com>
2018-04-08 20:05:30 +0300

    changed readme, added new .sh file

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Repository contains executable `gogs-shell.sh` file, which help to automate startup of your own instance [gogs](https://gogs.io)
 
+## The simplest way to startup gogs-server
+
 By default you can simply run:
 
 1. `git clone https://github.com/Dmitry-Kucher/gogs-docker-nginx.git`
@@ -9,5 +11,8 @@ By default you can simply run:
 1. `./gogs-shell.sh start`
 
  > **WARNING**: The simplest way will work only if you have no running any _webserver_ on `80` port
+ 
+ To stop `gogs-server` just run:
+ `./gogs-shell.sh stop` from `gogs-docker-nginx` path
  
  
---
+++ gogs-shell.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+initdevs() {
+    if [ ! -f ./config/devs-envs ]; then
+        echo "./config/devs-envs not found! Create file with DEVS environment variables"
+        exit 1
+    fi
+    . ./config/devs-envs
+}
+
+initpostgres() {
+    if [ ! -f ./config/postgres-envs ]; then
+        echo "./config/postgres-envs not found! Create file with POSTGRES environment variables"
+        exit 1
+    fi
+    . ./config/postgres-envs
+}
+
+initcustoms() {
+    if [ -f ./config/custom-envs ]; then
+        . ./config/custom-envs
+        echo "Custom ENVS loaded"
+    fi
+}
+
+initenvs() {
+    initdevs
+    initpostgres
+    initcustoms
+
+}
+
+startgogs() {
+    if [ ! -f ${DOCKER_COMPOSE_FILE} ]; then
+        echo "${DOCKER_COMPOSE_FILE} not found! Start gogs from directory with docker-compose files"
+        exit 1
+    fi
+    docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
+}
+
+stopgogs() {
+    docker-compose stop
+}
+
+initenvs
+
+case "$1" in
+    start)   startgogs ;;
+    stop)    stopgogs ;;
+    restart) stopme; startme ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
